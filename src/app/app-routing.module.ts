1b8f7e0228cd4c24e06e333758b0b03cf1166dd9
HamzaHasnaoui1 <hamza-hasnaoui@outlook.com>
2024-09-18 20:48:14 +0100

    first push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ListClientComponent} from "./client1/list-client/list-client.component";
+import {AddClientComponent} from "./client1/add-client/add-client.component";
+import {UpdateClientComponent} from "./client1/update-client/update-client.component";
+import {ListAchatComponent} from "./achat1/list-achat/list-achat.component";
+import {AddAchatComponent} from "./achat1/add-achat/add-achat.component";
+import {ListPaiementComponent} from "./paiement1/list-paiement/list-paiement.component";
+import {AddPaiementComponent} from "./paiement1/add-paiement/add-paiement.component";
+import {ListProduitComponent} from "./produit1/list-produit/list-produit.component";
+import {AddProduitComponent} from "./produit1/add-produit/add-produit.component";
+import {UpdateProduitComponent} from "./produit1/update-produit/update-produit.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: HomeComponent},
+  {path:'client', component:ListClientComponent},
+  {path:'addClient', component:AddClientComponent},
+  {path:'updateClient', component:UpdateClientComponent},
+  {path:'achat',component:ListAchatComponent},
+  {path:'addAchat',component:AddAchatComponent},
+  {path:'paiement',component:ListPaiementComponent},
+  {path:'addPaiement',component:AddPaiementComponent},
+  {path:'produit',component:ListProduitComponent},
+  {path:'addProduit',component: AddProduitComponent},
+  {path:'updateProduit',component: UpdateProduitComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddClientComponent } from './client1/add-client/add-client.component';
+import { UpdateClientComponent } from './client1/update-client/update-client.component';
+import { ListClientComponent } from './client1/list-client/list-client.component';
+import { ListProduitComponent } from './produit1/list-produit/list-produit.component';
+import { AddProduitComponent } from './produit1/add-produit/add-produit.component';
+import { UpdateProduitComponent } from './produit1/update-produit/update-produit.component';
+import { AddPaiementComponent } from './paiement1/add-paiement/add-paiement.component';
+import { ListPaiementComponent } from './paiement1/list-paiement/list-paiement.component';
+import { ListAchatComponent } from './achat1/list-achat/list-achat.component';
+import { AddAchatComponent } from './achat1/add-achat/add-achat.component';
+import { HomeComponent } from './home/home.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddClientComponent,
+    UpdateClientComponent,
+    ListClientComponent,
+    ListProduitComponent,
+    AddProduitComponent,
+    UpdateProduitComponent,
+    AddPaiementComponent,
+    ListPaiementComponent,
+    ListAchatComponent,
+    AddAchatComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
